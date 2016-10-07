dffe6047fd49c6ec651d9a33ae5318581bc86005
rikadixon <olya2008d@gmail.com>
2016-10-07 23:26:18 +0500

    Добавлен Паттерн immutable

--- Game_in_15/Game_in_15/Immutable_Game.cs
+++ Game_in_15/Game_in_15/Immutable_Game.cs
@@ -4,10 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Game_in_15
+namespace GameIn15
 {
-    class Immutable_Game
+  public  class Immutable_Game: Game
     {
+      public Immutable_Game(params int[] valuse):base(valuse)
+        {
+        }
         
+       Game SwapAndCheckFields(int x, int y, int x_0, int y_0)
+        {
+            
+            double check = Math.Abs(Math.Sqrt((x - x_0) * (x - x_0) + (y - y_0) * (y - y_0)));
+            int buffValue = this.field[x, y],
+                sizeOfFild= (int)Math.Sqrt(this.indexes.Length),
+                cout=0;
+            int[] value=new int[this.indexes.Length];
+            if ((x_0 <= field.Length) && (check == 1) && (y_0 <= field.Length) && (x_0 >= 0) && (y_0 >= 0))
+            { for (int i = 0; i < sizeOfFild; i++) {
+                    for(int j=0;j < sizeOfFild; j++)
+                    {
+                        if ((x == i) && (y == j))
+                        {
+                            value[cout] = 0;
+                        }
+                        if ((x_0 == i) && (y_0 == j))
+                        {
+                            value[cout] = buffValue;
+                        }
+                        else value[cout] = this.field[i, j];
+                        cout++;
+                    }
+                }
+                Game myGame = new Game(value);
+                return myGame;
+            }
+            else
+            {
+                throw new ArgumentException("Рядом нет нулей");
+            }
+        }
+        protected new Game Shift(int value)
+        {
+            Point valuePoint = GetLocation(value);
+            Point zeroPoint = GetLocation(0);
+            return SwapAndCheckFields(valuePoint.x, valuePoint.y, zeroPoint.y, zeroPoint.y);
+            ;
+        }
     }
 }
--- Game_in_15/Game_in_15/Program.cs
+++ Game_in_15/Game_in_15/Program.cs
@@ -3,35 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace GameIn15
 {
     class Program
     {
         static void Main(string[] args)
         { try
-            {   
+            {  
                 Game Fild = new Game(0, 7, 8, 1, 2, 3, 4, 5, 6);
                 //Console.WriteLine(Fild.field[0, 1]);
                 //Console.WriteLine(Fild[0, 1]);
+               
                 Game.Point value =Fild.GetLocation(4);
                 Console.WriteLine(value.x + " " + value.y);
+                Immutable_Game Game = new Immutable_Game(0, 7, 8, 1, 2, 3, 4, 5, 6);
                 for (int i = 0; i < 3; i++)
                 {
                     for (int j = 0; j < 3; j++)
                     {
-                        Console.Write(Fild[i, j] + " ");
+                        Console.Write(Game[i, j] + " ");
                     }
                     Console.WriteLine();
                 }
+                
                 Console.WriteLine("Новая");
-                Fild.Shift(1);
+                Game.Shift(1);
                
                 for (int i = 0; i < 3; i++)
                 {
                     for (int j= 0; j< 3; j++)
                     {
-                        Console.Write(Fild[i, j]+" ");
+                        Console.Write(Game[i, j]+" ");
                     }
                     Console.WriteLine();
                 }
