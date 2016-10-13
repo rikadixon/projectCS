ca35cd73cbe01ccc93acfdc31dfa5e11d58fad44
rikadixon <olya2008d@gmail.com>
2016-10-13 22:46:11 +0500

    Задание 3. Первый пункт

---
+++ RoughDesign/RoughDesign/Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoughDesign
+{
+   public class Charter
+    {
+        public string charterName { get; set; }
+        public double xp { get; protected set; }
+        public double mp { get; protected set; }
+        public double maxXP { get; protected set; }
+        public double maxMP { get; protected set; }
+        public double speedOfRegenXP { get; protected set; }
+        public double speedOfRegenMP { get; protected set; }
+        public List<Abilities> listOfAbilities{ get; protected set; }
+        void UseAbilitie(Charter userCharterr,int numberOfAbilitie)
+        {
+        }
+    }
+    public abstract class Abilities 
+    {
+       public virtual void Abilitie(Charter userCharter) { }
+    }
+    public class FireBall: Abilities
+    {
+        double xpDamage;
+        double mpCost;
+        readonly bool useOnYourself = false;
+        readonly bool useOnAnotherCharter = true;
+        public override void Abilitie(Charter userCharter)
+        {
+        }
+    }
+    public class Lightning : Abilities
+    {
+        double xpDamage;
+        double mpCost;
+        readonly bool useOnYourself = false;
+        readonly bool useOnAnotherCharter = true;
+        public override void Abilitie(Charter userCharter)
+        {
+        }
+    }
+    public class WindFlow : Abilities
+    {
+        double xpDamage;
+        double mpCost;
+        readonly bool useOnYourself = false;
+        readonly bool useOnAnotherCharter = true;
+        public override void Abilitie(Charter userCharter)
+        {
+        }
+    }
+    public class Regeneration : Abilities
+    {
+        double xpRegen;
+        double mpRegen;
+        readonly bool useOnYourself = true;
+        readonly bool useOnAnotherCharter = false;
+        public override void Abilitie(Charter userCharter)
+        {
+        }
+    }
+}
