9c7a8e30f513ff28c97a10b248d2b5a4fa104609
rikadixon <olya2008d@gmail.com>
2016-10-15 19:37:38 +0500

    Первый Пинпонг

--- RoughDesign/RoughDesign/Game.cs
+++ RoughDesign/RoughDesign/Game.cs
@@ -10,9 +10,12 @@ namespace RoughDesign
     {
 
     }
+    public interface IEnemyCharacter
+    {
 
+    }
 
-   public class Charter
+    public class Charter
     {
         public string charterName { get; set; }
         public double xp { get; protected set; }
@@ -21,7 +24,11 @@ namespace RoughDesign
         public double maxMP { get; protected set; }
         public double speedOfRegenXP { get; protected set; }
         public double speedOfRegenMP { get; protected set; }
+        public FrendlyCharters frends { get; protected set; }
+        public EnemyCharter enemies { get; protected set; }
         public List<Abilities> listOfAbilities{ get; protected set; }
+        public List<FrendlyCharters> listOfFrends { get; protected set; }
+        public List<EnemyCharter> listOfEnemis { get; protected set; }
         void UseAbilitie(List<Charter> anotherUserCharters, int numberOfAbilitie)
         {
         }
@@ -33,11 +40,32 @@ namespace RoughDesign
             listOfAbilities = listOfAbilitiesFromUser;
         }
     }
-    
+
+    public class FrendlyCharters:  IFrendlyCharacter
+    {   
+        public List<Charter> listOfFrends { get; protected set; }
+        FrendlyCharters(List<Charter> listOfFrends)
+        {
+            listOfFrends = new List<Charter>();
+            this.listOfFrends = listOfFrends;
+        }
+    }
+
+    public class EnemyCharter:IEnemyCharacter
+    {
+        public List<Charter> listOfEnemis { get; protected set; }
+        EnemyCharter(List<Charter> listOfEnemis)
+        {
+            listOfEnemis = new List<Charter>();
+            this.listOfEnemis = listOfEnemis;
+        }
+    }
+
     interface IAbilitiesOnYourself
     {
         void Abilitie();
     }
+
     interface IAbilitiesAnotherCharter
     {
         void Abilitie(Charter target);
@@ -47,6 +75,7 @@ namespace RoughDesign
     {
         double mpCost;
     }
+
     public class FireBall : Abilities, IAbilitiesAnotherCharter
     {
         double xpDamage;
@@ -61,6 +90,7 @@ namespace RoughDesign
 
         }
     }
+
     public class Lightning : Abilities, IAbilitiesAnotherCharter
     {
         double xpDamage;
@@ -75,6 +105,7 @@ namespace RoughDesign
 
         }
     }
+
     public class WindFlow : Abilities, IAbilitiesAnotherCharter
     {
         double xpDamage;
@@ -89,6 +120,7 @@ namespace RoughDesign
 
         }
     }
+
     public class Regeneration : Abilities, IAbilitiesOnYourself
     {   
         double xpRegen;
