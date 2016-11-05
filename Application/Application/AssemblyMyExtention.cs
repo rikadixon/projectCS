04f3668713bade1e1cc40dab76c916811ccbc10a
rikadixon <olya2008d@gmail.com>
2016-11-05 22:12:32 +0500

    Add files via upload

---
+++ Application/Application/AssemblyMyExtention.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Application
+{
+    public static class AssemblyMyExtention
+    {
+        public static List<Type> GetTypesInterface(this Assembly assembly, Type interfaceType)
+        {
+            return assembly.GetTypes().ToList().FindAll(type => type.GetInterfaces().Contains(interfaceType));
+        }
+    }
+}
---
+++ Application/Application/DirectoryInfoMyExtention.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Application
+{
+    public static class DirectoryMyInfoExtention
+    {
+        public static List<FileInfo> GetAllDllFiles(this DirectoryInfo directory)
+        {
+            var result = new List<FileInfo>();
+
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            FileInfo[] dllFiles = directory.GetFiles("*.dll");
+
+            result.AddRange(dllFiles);
+
+            foreach (var directoryInfo in directories)
+            {
+                result.AddRange(directoryInfo.GetAllDllFiles());
+            }
+
+            return result;
+        }
+
+        public static List<FileInfo> GetUniqueDllFiles(this DirectoryInfo directory)
+        {
+            var result = new List<FileInfo>();
+
+            var dllFiles = directory.GetAllDllFiles();
+
+            foreach (var dllFile in dllFiles)
+            {
+                var fileName = dllFile.Name;
+
+                List<string> filesName = result.Select(file => file.Name).ToList();
+
+                if (!filesName.Contains(fileName))
+                {
+                    result.Add(dllFile);
+                }
+            }
+
+            return result;
+        }
+    }
+   
+}
