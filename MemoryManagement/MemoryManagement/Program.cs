c64e25e2b7e990d6e1f831f8ed04a5608abfa41c
rikadixon <olya2008d@gmail.com>
2016-11-12 15:49:35 +0500

    1 часть задания

---
+++ MemoryManagement/MemoryManagement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MemoryManagement
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            var timer = new Timer();
+            using (timer.Start())
+            {
+                for (var i = 0; i < 25000; i++)
+                {
+                    for (var j = 0; j < 25000; j++)
+                    {
+                        var k = i + j;
+                    }
+                }
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            using (timer.Continue())
+            {
+                for (var i = 0; i < 25000; i++)
+                {
+                    for (var j = 0; j < 25000; j++)
+                    {
+                        var k = i + j;
+                    }
+                }
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ MemoryManagement/MemoryManagement/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MemoryManagement
+{
+    class Timer : IDisposable
+    {
+      private Stopwatch timerStopwatch;
+
+      public Timer()
+        {
+            timerStopwatch = new Stopwatch();
+        }
+      public void Dispose()
+        {
+            timerStopwatch.Stop();
+        }
+      public long ElapsedMilliseconds { get { return timerStopwatch.ElapsedMilliseconds; } }
+      public Timer Start(){
+            timerStopwatch.Reset();
+            timerStopwatch.Start();
+            return this;
+        }
+      public Timer Continue() {
+            timerStopwatch.Start();
+            return this;
+        }
+    }
+}
