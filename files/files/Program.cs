32c58fdbf1e3dc978bafe60be570f613734c95db
rikadixon <olya2008d@gmail.com>
2016-11-19 23:11:25 +0500

    Add files via upload
    1 и 2

---
+++ files/files/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Dynamic;
+
+namespace files
+{
+    public class Program
+    {
+        public static IEnumerable<string[]> ReadCsv1(string fileName)
+        {
+            using (var stream = new StreamReader(fileName))
+            {
+                string line;
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    yield return line.Replace("\"", "")
+                        .Split(',','.')
+                        .Select(str => str == "NA" ? null : str)
+                        .ToArray();
+                }
+
+                stream.Close();
+            }
+        }
+
+        public static IEnumerable<T> ReadCsv2<T>(string fileName)
+        {
+            var lines = ReadCsv1(fileName);
+            var fieldNames = lines.First();
+
+            lines = lines.Skip(1);
+
+            yield break;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
